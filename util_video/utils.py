47df3b8a4622fca209b4acf4d59ba54596b419cb
Hoang Nguyen <34732334+HoangNguyenM@users.noreply.github.com>
2024-10-28 06:15:07 -0400

    add utils for video processing

--- util_video/utils.py
+++ util_video/utils.py
@@ -1,1 +1,35 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# LICENSE file in the root directory of this source tree.
 
+# --------------------------------------------------------
+# References:
+# VideoMAE: https://github.com/MCG-NJU/VideoMAE
+# --------------------------------------------------------
+
+from torch.utils.data._utils.collate import default_collate
+
+def multiple_samples_collate(batch, fold=False):
+    """
+    Collate function for repeated augmentation. Each instance in the batch has
+    more than one sample.
+    Args:
+        batch (tuple or list): data batch to collate.
+    Returns:
+        (tuple): collated data batch.
+    """
+    inputs, labels, video_idx, extra_data = zip(*batch)
+    inputs = [item for sublist in inputs for item in sublist]
+    labels = [item for sublist in labels for item in sublist]
+    video_idx = [item for sublist in video_idx for item in sublist]
+    inputs, labels, video_idx, extra_data = (
+        default_collate(inputs),
+        default_collate(labels),
+        default_collate(video_idx),
+        default_collate(extra_data),
+    )
+    if fold:
+        return [inputs], labels, video_idx, extra_data
+    else:
+        return inputs, labels, video_idx, extra_data
